be37b5b192ef31cf04dec0ff079d28c4e2075901
Nilu <nlhpandey@gmail.com>
2012-09-10 22:50:44 -0400

    add stylesheets, layouts, partials, tests, and user signups

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Legalnote::Application.routes.draw do
-  root :to => 'static_pages#home'
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
