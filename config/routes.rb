7c2daa338f4c79d2375493eb9235367bd7b8a217
Nilu <nlhpandey@gmail.com>
2012-10-16 13:21:37 -0400

    Add demo page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Legalnote::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/demo',    to: 'static_pages#demo'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
